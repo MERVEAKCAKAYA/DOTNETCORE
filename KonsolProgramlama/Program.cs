c8a49ed64f25fca4bbe1c70cb66efd84f809bfd8
MERVEAKCAKAYA <72937913+MERVEAKCAKAYA@users.noreply.github.com>
2021-12-07 01:04:03 +0300

    Update Program.cs

--- KonsolProgramlama/Program.cs
+++ KonsolProgramlama/Program.cs
@@ -6,9 +6,23 @@ namespace KonsolProgramlama
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World2!");
-            Console.WriteLine("Hello World3!");
+            #ilk odev
+            Console.WriteLine("Bir sayi giriniz: ");
+            int sayi = Convert.ToInt32( Console.ReadLine());
+            int[] dizi= new int[sayi];
+            for(int i=0; i<sayi; i++)
+            {
+                Console.WriteLine((i+1)+". sayiyi giriniz: ");
+                dizi[i]= Convert.ToInt32( Console.ReadLine());
+
+            }
+            for(int j=0; j<dizi.Length; j++)
+            {
+                if(dizi[j]%2==0)
+                {
+                    Console.WriteLine(dizi[j]+" sayisi cift sayidir.");
+                }
+            }    
         }
     }
 }
